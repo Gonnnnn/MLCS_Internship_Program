76d39a94776eb41a4e9364aec6c4762bcdc6b468
Gonnnnn <geonkim@yonsei.ac.kr>
2021-08-02 16:46:30 +0900

    got CNN complete

--- 002_Neural_Networks/002_CNN/cnn_network.py
+++ 002_Neural_Networks/002_CNN/cnn_network.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50V2
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
-
+from tensorflow.keras.regularizers import l2
 
 class cifar_Res():
     def __init__(self):
@@ -9,18 +9,17 @@ class cifar_Res():
 
     def build(self):
         IMG_SHAPE = (32, 32, 3)
-        input_tensor = tf.keras.Input(shape = (224, 224))
+        input_tensor = tf.keras.Input(shape = IMG_SHAPE)
         resized = tf.keras.layers.Lambda(lambda image : tf.image.resize(image, (224, 224)))(input_tensor)
-        res = ResNet50V2(input_shape=IMG_SHAPE, include_top=False, pooling = 'max', weights='imagenet', )
+        res = ResNet50V2(input_shape=IMG_SHAPE, input_tensor=resized, include_top=False, pooling = 'max', weights='imagenet')
 
-        for layer in res:
+        for layer in res.layers:
             layer.trainable = False
 
         flatten = Flatten()(res.output)
 
 
         def fc(num_classes, _input, activation, trainable):
-            l2 = tf.keras.regularizers.l2()
             a = _input
             a = Dense(512, kernel_regularizer = l2(0.001), trainable = trainable)(a)
             a = BatchNormalization(trainable = trainable)(a)
@@ -32,7 +31,7 @@ class cifar_Res():
             a = Activation('relu', trainable=trainable)(a)
             a = Dropout(0.3)(a)
 
-            return Dense(num_classes, activation = activation, trainable = trainable)
+            return Dense(num_classes, activation = activation, trainable = trainable)(a)
 
         prediction = fc(10, flatten, 'softmax', True)
         model = tf.keras.models.Model(inputs = res.input, outputs = prediction)
--- 002_Neural_Networks/002_CNN/train.py
+++ 002_Neural_Networks/002_CNN/train.py
@@ -5,18 +5,21 @@ from tensorflow.keras.utils import to_categorical
 import numpy as np
 import random
 
-batch_size = 128
-epochs = 10
+batch_size = 512
+epochs = 5
 train_ratio = 0.7
 val_ratio = 0.15
 
 def cifar10_data_loader(train_ratio, val_ratio, shuffle = False):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = tf.keras.applications.resnet_v2.preprocess_input(x_train)
+    y_train = to_categorical(y_train, 10)
+    x_test = tf.keras.applications.densenet.preprocess_input(x_test)
+    y_test = to_categorical(y_test, 10)
 
     x_tot = np.concatenate((x_train, x_test), axis=0)
-    # since /= is not allowed to change the data type of an instance, we have to change it w astype in advance
-    x_tot = x_tot.astype('float32')
     y_tot = np.concatenate((y_train, y_test), axis=0)
+
 
     if shuffle == True:
         joined = list(zip(x_tot, y_tot))
@@ -26,14 +29,8 @@ def cifar10_data_loader(train_ratio, val_ratio, shuffle = False):
         y_tot = list(y_tot_temp)
 
     x_train, x_val, x_test = np.split(x_tot, [int(train_ratio * len(x_tot)), int((train_ratio + val_ratio) * len(x_tot))])
-    x_train /= 255
-    x_val /= 255
-    x_test /= 255
-
     y_train, y_val, y_test = np.split(y_tot, [int(train_ratio * len(y_tot)), int((train_ratio + val_ratio) * len(y_tot))])
-    y_train = tf.keras.utils.to_categorical(y_train, 10)
-    y_val = tf.keras.utils.to_categorical(y_val, 10)
-    y_test = tf.keras.utils.to_categorical(y_test, 10)
+
 
     return x_train, x_val, x_test, y_train, y_val, y_test
 
